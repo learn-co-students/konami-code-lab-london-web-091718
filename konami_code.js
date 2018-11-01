f26510ea931c430d730cb07fd0e8ad06adf35b14
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-11-01 18:15:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,23 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
-  // your code here
+  let body = document.querySelector('body')
+  let index = 0
+  const konami = (e) => {
+    let key = e.key
+    console.log(key)
+    if (key === codes[index]) {
+      index++;
+      if (index === codes.length) {
+        alert("Hurray!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
+  body.addEventListener('keydown', konami)
 }
