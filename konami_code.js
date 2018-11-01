8dfd637d29d314e032178b8bc7cb85b97753675b
Amalie Smidth <amaliesmidth@gmail.com>
2018-11-01 15:57:54 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,16 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0
+
+  document.body.addEventListener('keydown', (event) => {
+    const key = event.key
+
+    index = (codes[index] === key) ? ++index : 0
+
+    if (index === codes.length) {
+      window.alert("hi there you fool")
+      index = 0
+    }
+  })
 }
