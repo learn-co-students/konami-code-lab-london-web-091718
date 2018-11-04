27b24d8f5cb3c9cfb9c93308904c629368851437
BarneyLoosemore <biscuitfull@gmail.com>
2018-11-04 19:44:38 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,19 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  const body = document.querySelector('body');
+  const code_string = [];
+  body.addEventListener('keydown', function (e) {
+    code_string.push(e.key);
+    console.log(e.key);
+    console.log(code_string);
+    if (code_string.toString() === codes.toString()) {
+      alert("nice");
+    }
+    if (code_string.length > 9) {
+      code_string.shift();
+    }
+  })
 }
+
+init();
